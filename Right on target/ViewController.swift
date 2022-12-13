98de3e12051dc36cefedfebbda38b6db33a19bb0
Dmitry <borodulkin_d@mail.ru>
2022-12-13 20:29:08 +0300

    Версия проекта 1.2 готова в полном объеме.

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Сущность "Игра"
+    var game: Game!
+    
+    // Элементы на сцене
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var label: UILabel!
+    
+    // MARK: - Жизненный цикл
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        game = Game(startValue: 1, endValue: 50, rounds: 5)
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
     }
+    
+    // MARK: - Взаимодействие View - Model
+    @IBAction func checkNumber() {
+        game.calculateScore(with: Int(slider.value))
+        if game.isGameEnded {
+            showAlertWith(score: game.score)
+            game.restartGame()
+        } else {
+            game.startNewRound()
+        }
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
+        }
+    
+    // MARK: - Обновление View
+    private func updateLabelWithSecretNumber(newText: String) {
+        label.text = newText
+    }
+    
+    private func showAlertWith(score: Int) {
+        let alert = UIAlertController(
+            title: "Игра окончена",
+            message: "Вы заработали \(score) очков",
+            preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+        }
+    }
+    
 
 
-}
 
